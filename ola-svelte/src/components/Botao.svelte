95ef2961d24fffeb696f93704cbe866008ff78f5
Kinnitchi <igorivieira97@gmail.com>
2022-04-18 14:52:43 -0300

    componentes de Botao Calculadora e Display

---
+++ ola-svelte/src/components/Botao.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export let texto: string
+  export let triple: boolean = false;
+  export let duplo: boolean = false; 
+  export let operacao: boolean = false; 
+  export let destaque: boolean = false; 
+</script>
+
+<button class="botao" 
+  class:triple class:duplo class:operacao class:destaque>
+  {texto}
+</button>
+
+<style>
+  .botao{
+    margin: 0;
+    border: 1px solid var(--bg);
+    flex-basis: 25%;
+    background-color: #404040;
+    color: #fff;
+    cursor: pointer;
+  }
+
+  .triple{
+    flex-basis: 75%;
+  }
+  .duplo{
+    flex-basis: 50%;
+  }
+  .operacao{
+    background-color: #333333;
+    color: #fff;
+  }
+  .destaque{
+    background-color: #d43e3e;
+    color: #fff;
+  }
+</style>
---
+++ ola-svelte/src/components/Display.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let valor: string
+
+</script>
+
+<div class="display">
+  <span>{valor}</span>
+</div>
+
+<style>
+  .display{
+    display: flex;
+    flex: 1;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 0px 5px;
+    font-size: 1.5rem;
+    background-color: aliceblue;
+    overflow: hidden;
+  }
+</style>
